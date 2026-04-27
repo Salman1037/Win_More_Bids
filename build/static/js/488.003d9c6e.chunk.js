ee45c0244762578d726ff90ec72f10bf8e827476
Salman1037 <zakka727@gmail.com>
2026-04-27 18:04:51 +0500

    fixed build issues

---
+++ build/static/js/488.003d9c6e.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(globalThis.webpackChunkwin_bids_co=globalThis.webpackChunkwin_bids_co||[]).push([[488],{2488(e,i,s){new WeakMap;new Set}}]);
+//# sourceMappingURL=488.003d9c6e.chunk.js.map
